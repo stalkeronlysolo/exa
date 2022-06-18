48e5c4821e1f254cec1a6ffe86a46d75eca4066c
Kalo <kaloianovk@gmail.com>
2022-06-18 04:16:34 +0300

    second commit

--- contracts/nexo.sol
+++ contracts/nexo.sol
@@ -252,15 +252,12 @@ contract Nexo{
     mapping(address => uint) public userUsdcBalance;
     uint public strategyEthBalance;
     uint256 private minimumDepositEth = 9000000000000000;
-
     bool private shutDown = false;
-
     address admin;
     address private USDC_token = 0x07865c6E87B9F70255377e024ace6630C1Eaa37F;
     address private DAI_token = 0xaD6D458402F60fD3Bd25163575031ACDce07538D; //DAI Ropsten
     address private WETH = 0x0a180A76e4466bF68A7F86fB029BEd3cCcFaAac5; //WETH Address
     address private routerUniswap= 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D; //WETH Address
-
     uint private uniswapFee = 3; //uniswap fee  0.3%
     uint private strategyFee = 500; // withdraw fee for 50% easier to tesdt with large perc
 
@@ -291,18 +288,19 @@ contract Nexo{
     function swapETHToUSDC() external payable
     {       
         
-        // refund leftover ETH to user
-        //(bool success,) = msg.sender.call{ value: address(this).balance }("");
-        //require(success, "refund failed");
+        //This is the main function. The user makes a deposit which is converted to DAI/USDC
+
         require(!shutDown, "Sorry the contract is shutdown. You can withdraw your deposits but new ones are disabled.");
+
+         // ETH amount should be > than admin set minimum
         require(msg.value > minimumDepositEth, "Minimum ETH amount should be > 0.009");
-        // ETH amount should be > 0
+       
            
         address[] memory path = new address[](2);
         path[0] = router.WETH();
         path[1] = address(DAI_token);
 
-        //USDC_token.approve(address(router), amount);
+       
         uint[] memory tokens=router.swapExactETHForTokens{value:msg.value}(       
                 0,
                 path,
@@ -311,28 +309,32 @@ contract Nexo{
             );
         uint256 outputTokenCount = uint256(tokens[tokens.length - 1]);
 
+        //here i need to redirect the thing to aave.
+
         userEthBalance[msg.sender] += discountFee(msg.value,uniswapFee); //we paid a 0.30% fee when swaping eth for usdc so removing from the final amount
         strategyEthBalance += discountFee(msg.value,uniswapFee); //we paid a 0.30% fee when swaping eth for usdc so removing from the final amount
         userUsdcBalance[msg.sender] +=outputTokenCount; 
 
-        //here i need to redirect the thing to aave.
+        
        
         }
 
 
     function witdrawPartialStake(uint amountEth) external payable { 
 
-        ///this case is for all tokens. latter CHANGE FOR CUSTOM AMOUNTS!!!
+        ///partial decrase stake
         
         //require(userEthBalance[msg.sender] > 0, "Can not withdraw. You do not have any funds deposited in the strategy");
 
         require(amountEth <= userEthBalance[msg.sender], "You can not withdraw more than what you staked. "); //this is not 100% correct but i m running out of time.
         uint tokenMaxWithdrawAmount= userUsdcBalance[msg.sender];
+
+        //here i need to take back what user has to aave.
+
          //aprove
         IERC20 dai = IERC20(DAI_token);
 
-        //dai.approve(routerUniswap, userUsdcBalance[msg.sender]); 
-        //require(daiToken.transferFrom(myAccount, address(this), daiAmountIn), "transferFrom failed");
+       
         
         require(dai.approve(address(routerUniswap), 0), "approve failed 1");
         require(dai.approve(address(routerUniswap), tokenMaxWithdrawAmount), "approve failed 2 ");
@@ -340,10 +342,7 @@ contract Nexo{
         address[] memory path = new address[](2);
         path[0] = address(DAI_token);
         path[1] = router.WETH();
-
-        
-    
-
+         
         uint[] memory tokens  = router.swapTokensForExactETH(
                 amountEth,
                 tokenMaxWithdrawAmount,
@@ -352,11 +351,9 @@ contract Nexo{
                 block.timestamp
             );
 
-        
-
-               
-         uint256 outputTokenCount1 = uint256(tokens[1]);
-         uint256 outputTokenCount2 = uint256(tokens[0]);
+                       
+        uint256 outputTokenCount1 = uint256(tokens[1]);
+        uint256 outputTokenCount2 = uint256(tokens[0]);
 
         userEthBalance[msg.sender] -=  PlusFee(outputTokenCount1,uniswapFee); //we got that ethereum but we also paid a fee so add the fee to match what the user has
         strategyEthBalance -= PlusFee(outputTokenCount1,uniswapFee);//we got that ethereum but we also paid a fee so add the fee to match what the user has
@@ -368,7 +365,7 @@ contract Nexo{
 
         function witdrawFullStake() external payable { 
 
-        ///this case is for all tokens. latter CHANGE FOR CUSTOM AMOUNTS!!!
+        ///for easysness withdraw of all user funds without specifing amount
         
         require(userEthBalance[msg.sender] > 0, "Can not withdraw. You do not have any funds deposited in the strategy");
 
@@ -387,7 +384,7 @@ contract Nexo{
 
 
 
-        //USDC_token.approve(address(router), amount);
+        
         uint[] memory tokens=router.swapExactTokensForETH(       
                 tokenwithdrawAmount,
                 0,
@@ -405,20 +402,24 @@ contract Nexo{
     }
 
     function getNexoBalance () public view returns (uint) {
+        //contract balance
         return address(this).balance;
     }
 
      function getUserEthBalance () public view returns (uint) {
+         //user estimate eth balance
         return userEthBalance[msg.sender];
     }
 
     function getStrategyEthBalance () public view returns (uint) {
+        //strategy total eth balance
         return strategyEthBalance;
 
 
     }
       
     function discountFee(uint amount, uint fee) public pure returns (uint) {
+        //used to discount fees from an amount
         
         uint finalFee = amount.mul(1000-fee).div(1000);
 
@@ -426,6 +427,8 @@ contract Nexo{
     }
 
     function PlusFee(uint amount, uint fee) public pure returns (uint) {
+
+        //add the fee to an amount (when i get eth amounts from uniswap which have been charged with 0.3%
         
         uint finalFee = amount.mul(1000+fee).div(1000);
 
@@ -433,6 +436,7 @@ contract Nexo{
     }
 
     function shutDownContract() public onlyAdmin {
+        //shut down irreversible does not accept deposits
         require(!shutDown, "Already shutdown");
         shutDown=true;
 
@@ -440,6 +444,7 @@ contract Nexo{
 
     function isshutDown() public view returns(bool)
     {
+        //check if the contract is shut down
         return shutDown; //is contract shut down for new deposits?
     }
 
--- test/nexo.js
+++ test/nexo.js
@@ -1,14 +1,70 @@
 const { expect } = require("chai");
 
-describe("Nexo defi contract", function () {
-  it("Get Balance contract", async function () {
-    const [owner] = await ethers.getSigners();
 
-    const Nexo = await ethers.getContractFactory("Nexo");
+describe("Hi this is Nexo test defi contract", function ()  {
 
-    const hardhatNexo = await Nexo.deploy();
+    let Nexo, myNexo, owner, addr1, addr2;
+ 
+    beforeEach ( async () => {
 
-    const ownerBalance = await hardhatNexo.getNexoBalance();
+      
+      const Nexo = await ethers.getContractFactory("Nexo");
+      myNexo = await Nexo.deploy();
+      
+
+    })
+     it("Making a deposit of 0.1 ethereum which will be swapped to DAI on Uniswap.", async function () {
+
+      const [owner] = await ethers.getSigners();
+
+       const deposit1Eth = await myNexo.connect(owner).swapETHToUSDC({value: ethers.utils.parseUnits("0.1", "ether").toHexString()});
    
-  });
+    })
+
+    it("Cheking Strategy Balance", async function () {
+
+        const strategyBalance = await myNexo.getStrategyEthBalance();
+
+    
+
+    //console.log("Strategy Balance : ",strategyBalance);// User Eth deposits ;", userETHBalance); //returns big number 
+   
+    })
+
+    it("Cheking User Eth Balance", async function () {
+
+        const userEthBalance = await myNexo.getStrategyEthBalance();
+
+    
+    //console.log("User Eth Balance : ",formatEther(userEthBalance,18));// User Eth deposits ;", userETHBalance); //returns big number 
+   
+    })
+      it("Initiating IRREVERSIBLE contract shutdown ", async function () {
+
+        const WithdrawEth = await myNexo.connect(owner).shutDownContract();
+
+    
+
+    //console.log("User Eth Balance : ",formatEther(userEthBalance,18));// User Eth deposits ;", userETHBalance);
+   
+    })
+
+
+
+
+      it("Withdrawing/decrasing Stake from the contract by ETH 0.01(Converted from DAI to ETH).", async function () {
+        const [owner] = await ethers.getSigners();
+
+        const WithdrawEth = await myNexo.connect(owner).witdrawPartialStake(ethers.utils.parseUnits("0.01", "ether").toHexString());
+
+    
+
+    //console.log("User Eth Balance : ",formatEther(userEthBalance,18));// User Eth deposits ;", userETHBalance);
+   
+    })
+
+     
+
+
+
 });
